4a199d47469aa87953a79d6d729f0c001d6a4fb7
vaniakazeko <vania.kazeko.00@bk.ru>
2023-04-13 11:28:30 +0300

    add test_app application

--- backend/efood/efood/settings.py
+++ backend/efood/efood/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'test_app.apps.TestAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
