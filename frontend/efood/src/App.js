e92f2cfcbb1a8aa5d9c347056d985cf9fcf21caf
vaniakazeko <vania.kazeko.00@bk.ru>
2023-04-17 22:11:36 +0300

    edit

--- frontend/efood/src/App.js
+++ frontend/efood/src/App.js
@@ -5,7 +5,7 @@ function BookList() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/v1/books/')
+    axios.get('http://127.0.0.1:8000')
       .then(response => {
         setTasks(response.data);
       })
