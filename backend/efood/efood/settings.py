f22f4a8511c24ea356270eb2e3075e2f3dc84df6
vaniakazeko <vania.kazeko.00@bk.ru>
2023-04-17 22:37:07 +0300

    db

--- backend/efood/efood/settings.py
+++ backend/efood/efood/settings.py
@@ -10,7 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+env = environ.Env(
+    DATABASE_NAME=(str,),
+    DATABASE_USER=(str,),
+    DATABASE_PASSWORD=(str,),
+    DATABASE_HOST=(str,),
+    DATABASE_PORT=(str,),
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +89,11 @@ WSGI_APPLICATION = 'efood.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'efood_db',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env('DATABASES_NAME'),
+        'USER': env('DATABASES_USER'),
+        'PASSWORD': env('DATABASES_PASSWORD'),
+        'HOST': env('DATABASES_HOST'),
+        'PORT': env('DATABASES_PORT'),
     }
 }
 
