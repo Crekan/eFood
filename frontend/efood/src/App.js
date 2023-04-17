6396b9fbc733a6eddf598c7801330a79aaa3f9f2
vaniakazeko <vania.kazeko.00@bk.ru>
2023-04-17 22:53:17 +0300

    js

--- frontend/efood/src/App.js
+++ frontend/efood/src/App.js
@@ -30,32 +30,3 @@ function BookList() {
 }
 
 export default BookList;
-
-
-
-// function PostList() {
-//   const [tasks, setTasks] = useState([]);
-//
-//   useEffect(() => {
-//     axios.get('http://127.0.0.1:8000/api/v1/books/')
-//       .then(response => {
-//         setTasks(response.data);
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   }, []);
-//
-//   return (
-//     <div>
-//       <h1>Posts</h1>
-//       <ul>
-//         {tasks.map(task => (
-//           <li key={task.id}>{task.title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-//
-// export default PostList;
